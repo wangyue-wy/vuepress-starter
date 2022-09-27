b434adefafc046de39b47ff61d0e79de97f24943
wangyue <837977957@qq.com>
2022-09-27 18:54:17 +0800

    wy

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@
 set -e
 
 # 生成静态文件
-npm vuepress build
+npx vuepress build
 
 # 进入生成的文件夹
-cd doc/.vuepress/dist
+cd docs/.vuepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git push -f git@github.com:wangyue-wy/vuepresstarter.github.io.git master:gh-pages
+git push -f git@github.com:wangyue-wy/vuepress-starter.git master:gh-pages
 
 cd -
